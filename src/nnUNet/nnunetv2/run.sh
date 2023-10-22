e97219381fc6be1e596b8e851ed8323e39789a52
joelive <linkwj@qq.com>
2023-10-23 06:11:21 +0800

    update

---
+++ src/nnUNet/nnunetv2/run.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 706 3d_fullres 4
+CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 706 3d_fullres 1
+CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 706 3d_fullres 2
+CUDA_VISIBLE_DEVICES=1 nnUNetv2_train 706 3d_fullres 3
---
+++ src/nnUNet/nnunetv2/runnnFormer_v2.sh
@@ -0,0 +1,5 @@
+#CUDA_VISIBLE_DEVICES=0 nnUNetv2_train -tr nnFormerTrainer 706 3d_fullres 0
+#CUDA_VISIBLE_DEVICES=0 nnUNetv2_train  -tr nnFormerTrainer 706 3d_fullres 4
+#CUDA_VISIBLE_DEVICES=0 nnUNetv2_train   -tr nnFormerTrainer 706 3d_fullres 1
+CUDA_VISIBLE_DEVICES=2 nnUNetv2_train --c  -tr nnFormerTrainer 706 3d_fullres 2
+CUDA_VISIBLE_DEVICES=2 nnUNetv2_train --c -tr nnFormerTrainer 706 3d_fullres 3
